0de37131e70f923c030acb08801d865c2830d2ab
Jacekortez <167092742+Jacekortez@users.noreply.github.com>
2024-07-01 17:03:10 +0300

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ const chalk = require('chalk');
 
 // Other
 global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.linkGroup = 'https://chat.whatsapp.com/G5HYXX6p9gIJzG3YkhMrSK'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
 global.ownername = "JΛϾΞ";
@@ -29,16 +29,16 @@ global.autoBlock = true
  
 /*REPLY MESSAGE*/
 global.mess = {
-    banned: '`You are *Banned* fron using commands!`',
-    bangc: '`This Group is *Banned* from using Commands!`',
+    banned: '`yr bannisshhheeedd fool`',
+    bangc: '`i cant work for u low lifes`',
     success: '`✓ Success`',
-    admin: '`This feature is only for group admins`',
+    admin: '`only for the chosen ones u fool😥`',
     botAdmin: '`I am not an admin!`',
     owner: '`You are not my owner`',
-    group: '`You can use this command only in groups ❌`',
-    private: '`Feature is used only for private chats!`',
+    group: '`type shii😂😂`',
+    private: '`only for yr dms`',
     bot: '`Bot number user special features`',
-    wait: '*Processing Your request*',
+    wait: '*chillax bro im doin it*',
     endLimit: '`Your daily limit has expired, the limit will be reset every 12 hours`',
 };
 
